61f995aae735fb0ab4611a07a43859690d077250
domm <dommferger@gmail.com>
2023-08-21 19:14:21 -0400

    Took me a min

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let message
+
+function scuberGreetingForFeet(height, message){
+  if(height <= 400){
+    message = ('This one is on me!');
+      } else if (height >= 401 && height <= 2000){
+        message = ('That will be twenty bucks.');
+          } else if (height >=2001 && height <= 2500){
+          message = ('I will gladly take your thirty bucks.');
+            } else if (height >= 2501){
+                message = 'No can do.';
+                  }
+                  return message
+ }
+ scuberGreetingForFeet(199);
+
+
+function ternaryCheckCity(city){
+  if(city === "NYC"){
+    message = ('Ok, sounds good.');
+  } else {
+    message = ('No go.');
+  }
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    if (tip === "generous"){
+      message = ('Thank you so much.');
+    } else if (tip === "not as generous"){
+      message = ('Thank you.');
+    } else {
+        message = ('Bye.');
+    }
+  
+  return message;
 }
